8749c5fbcf54868428f9d54876fa3c381f9d1d51
Adnan <adnanmunir83@gmail.com>
2018-11-12 19:37:08 +0400

    Bug Fix for Materila Request

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -198,18 +198,18 @@ class MaterialRequest(BuyingController):
 		if self.material_request_type == "Purchase":
 			return
 
-		req_qty=0
+        if self.material_request_type in ("Material Transfer"):
+             for d in self.get("items"):
+                 req_qty = flt(frappe.db.sql("""select ifnull(sum(qty),0) from `tabMaterial Request Item`
+		             where docstatus=1 and item_code= %s and parent != %s
+		            and sales_order = %s """,(d.item_code, self.name, d.sales_order))[0][0])
+                 sales_order_qty = flt(frappe.db.sql("select qty from `tabSales Order Item` where name= %s ",
+                                                (d.sales_order_item))[0][0])
+                if (req_qty + d.qty) - sales_order_qty > 0:
+                    frappe.throw(_("The total Requested quantity {0}   \
+		            cannot be greater than Sales Order quantity {1} , for Item {2}").format(d.qty, sales_order_qty,
+                                                                                            d.item_code))
 
-		if self.material_request_type in ("Material Transfer"):
-			for d in self.get("items"):
-				req_qty =  flt(frappe.db.sql("""select (select qty from `tabSales Order Item` where name= %s )- ifnull(sum(qty),0) from `tabMaterial Request Item`
-					where docstatus=1 and item_code= %s and parent != %s
-					and sales_order = %s """,
-					(d.sales_order_item,d.item_code,self.name, d.sales_order))[0][0])
-
-				if req_qty and (req_qty - d.qty) < 0:
-					frappe.throw(_("The total Requested quantity {0}   \
-						cannot be greater than Sales Order quantity for Item {1}").format( d.qty, d.item_code))
 
 def update_completed_and_requested_qty(stock_entry, method):
 	if stock_entry.doctype == "Stock Entry":
