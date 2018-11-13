24f44c0f3b450bf199ffd8a42dfad1d07a1b8b5b
Adnan <adnanmunir83@gmail.com>
2018-11-13 19:22:29 +0400

    Indicator at Sales Invoice

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.js
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.js
@@ -13,6 +13,10 @@ erpnext.accounts.SalesInvoiceController = erpnext.selling.SellingController.exte
 	setup: function(doc) {
 		this.setup_posting_date_time_check();
 		this._super(doc);
+		this.frm.set_indicator_formatter('item_code',
+		function(doc) {
+				return (doc.docstatus==1 || doc.qty <= doc.actual_qty) ? "green" : "orange"
+			})
 	},
 	onload: function() {
 		var me = this;
