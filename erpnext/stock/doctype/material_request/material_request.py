e24b85a57ac23a57b396434ea4bd477d680cb311
Adnan <adnanmunir83@gmail.com>
2019-01-12 00:52:13 +0400

    MR fix witout sales order submission

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -200,15 +200,16 @@ class MaterialRequest(BuyingController):
 
 		if self.material_request_type in ("Material Transfer"):
 			for d in self.get("items"):
-				req_qty = flt(frappe.db.sql("""select ifnull(sum(qty),0) from `tabMaterial Request Item`
+				if d.sales_order and d.sales_order  != "0":
+					req_qty = flt(frappe.db.sql("""select ifnull(sum(qty),0) from `tabMaterial Request Item`
 		                 where docstatus=1 and item_code= %s and parent != %s
 		                and sales_order = %s """,(d.item_code, self.name, d.sales_order))[0][0])
-				sales_order_qty = flt(frappe.db.sql("select qty from `tabSales Order Item` where name= %s ",
+					sales_order_qty = flt(frappe.db.sql("select qty from `tabSales Order Item` where name= %s ",
                                                 (d.sales_order_item))[0][0])
-				if ((req_qty + d.qty) - sales_order_qty) > 0:
-					frappe.throw(_("The total Requested quantity {0}   \
-		                cannot be greater than Sales Order quantity {1} , for Item {2}")
-                        .format(d.qty, sales_order_qty, d.item_code))
+					if ((req_qty + d.qty) - sales_order_qty) > 0:
+						frappe.throw(_("The total Requested quantity {0}   \
+		                	cannot be greater than Sales Order quantity {1} , for Item {2}")
+                        	.format(d.qty, sales_order_qty, d.item_code))
 
 
 def update_completed_and_requested_qty(stock_entry, method):
