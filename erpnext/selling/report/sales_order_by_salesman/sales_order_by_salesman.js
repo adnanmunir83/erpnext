5c428127f191dddb63371e240a06d814a638ddc9
Saif <saifi0102@gmail.com>
2019-01-19 04:54:34 +0500

    feat: Sales Order by Salesman script report (#11)

---
+++ erpnext/selling/report/sales_order_by_salesman/sales_order_by_salesman.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Sales Order By Salesman"] = {
+    "filters": [
+        {
+            "fieldname":"fdate",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+             "default": get_today(),
+            "reqd": 1
+        } ,
+        {
+            "fieldname":"tdate",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+             "default": get_today(),
+            "reqd": 1
+        }
+    ]
+}
