4e7756dfa01de8a977bb85735007349197d5002e
Adnan <adnanmunir83@gmail.com>
2019-02-09 23:16:11 +0400

    Add Item price if it dosent exsist item label

--- erpnext/stock/doctype/item_label/item_label.py
+++ erpnext/stock/doctype/item_label/item_label.py
@@ -7,4 +7,16 @@ import frappe
 from frappe.model.document import Document
 
 class ItemLabel(Document):
-	pass
+
+	def on_submit(self):
+		for d in self.get("items"):
+			if not frappe.db.get_value("Item Price", {"item_code": d.item_code,"price_list":d.price_list}):
+				self.make_item_price(d.item_code,d.price_list,d.item_price)
+
+	def make_item_price(self,item, price_list_name, item_price):
+		frappe.get_doc({
+			"doctype": "Item Price",
+			"price_list": price_list_name,
+			"item_code": item,
+			"price_list_rate": item_price
+		}).insert(ignore_permissions=True)
