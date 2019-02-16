4e89416f660c1d9565425074133050356cd2fe83
Adnan <adnanmunir83@gmail.com>
2019-02-16 19:15:24 +0400

    Stock Summary report fixed

--- erpnext/stock/report/stock_ledger/stock_ledger.js
+++ erpnext/stock/report/stock_ledger/stock_ledger.js
@@ -7,8 +7,7 @@ frappe.query_reports["Stock Ledger"] = {
 			"fieldname":"company",
 			"label": __("Company"),
 			"fieldtype": "Link",
-			"options": "Company",
-			"default": frappe.defaults.get_user_default("Company"),
+			"options": "Company",			
 			"reqd": 1
 		},
 		{
