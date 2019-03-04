b8c29cc06454ca709a5daa27c6b98fbf92804e56
Adnan <adnanmunir83@gmail.com>
2019-03-04 18:40:32 +0400

    Payment Entry will not fetch Sales order

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -552,7 +552,7 @@ def get_outstanding_reference_documents(args):
 		orders_to_be_billed =  get_orders_to_be_billed(args.get("posting_date"),args.get("party_type"),
 			args.get("party"), party_account_currency, company_currency)
 
-	return negative_outstanding_invoices + outstanding_invoices + orders_to_be_billed
+	return negative_outstanding_invoices + outstanding_invoices #+ orders_to_be_billed
 
 
 def get_orders_to_be_billed(posting_date, party_type, party, party_account_currency, company_currency):
