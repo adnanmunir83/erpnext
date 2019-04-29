62bb8524ce74757683018f9538be09bf076c73de
Adnan <adnanmunir83@gmail.com>
2019-04-30 00:31:24 +0400

    Bypass Transit and Direct Transfer check box

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -896,32 +896,34 @@ class StockEntry(StockController):
 			stock_bin.update_reserved_qty_for_sub_contracting()
 	
 	def validate_permissions(self):
-		# Check for Source and Target Warehouse are either normal or brerakage
-		for d in self.get("items"):
-			if ("Breakage" in d.t_warehouse and "Breakage" not in d.s_warehouse ):
-				frappe.throw(_("Normal Warehouse to Breakage Transfer is not possible for Item {0} .").format(d.item_code))
+		if not self.bypass_transit_warehouse :
+			# Check for Source and Target Warehouse are either normal or brerakage
+			for d in self.get("items"):
+				if ("Breakage" in d.t_warehouse and "Breakage" not in d.s_warehouse ):
+					frappe.throw(_("Normal Warehouse to Breakage Transfer is not possible for Item {0} .").format(d.item_code))
 
-			if ("Breakage" in d.s_warehouse and "Breakage" not in d.t_warehouse ):
-				frappe.throw(_("Breakage Warehouse to Normal Transfer is not possible for Item {0} .").format(d.item_code))
-		# Get User's Warehouse
-		
-		#check if user is owner of this warehouse
+				if ("Breakage" in d.s_warehouse and "Breakage" not in d.t_warehouse ):
+					frappe.throw(_("Breakage Warehouse to Normal Transfer is not possible for Item {0} .").format(d.item_code))
+			# Get User's Warehouse
+			
+			#check if user is owner of this warehouse
 
 	def validate_user_warehouse(self):
-		user_warehouse = frappe.db.get_value("User", {"name": frappe.session.user}, "user_warehouse")	
-		strTransit = "Transit"
-		if user_warehouse :	
-			for d in self.get("items"):				
-				if ( strTransit in d.t_warehouse ):
-					if not (user_warehouse in d.s_warehouse or user_warehouse.replace("Normal","Breakage") in d.s_warehouse ):
-						frappe.throw(_("you are note allowed to tranfer stock from warehouse {0} .").format(d.s_warehouse))
+		if not self.bypass_transit_warehouse :
+			user_warehouse = frappe.db.get_value("User", {"name": frappe.session.user}, "user_warehouse")	
+			strTransit = "Transit"
+			if user_warehouse :	
+				for d in self.get("items"):				
+					if ( strTransit in d.t_warehouse ):
+						if not (user_warehouse in d.s_warehouse or user_warehouse.replace("Normal","Breakage") in d.s_warehouse ):
+							frappe.throw(_("you are note allowed to tranfer stock from warehouse {0} .").format(d.s_warehouse))
+				
+					if ( strTransit in d.s_warehouse):
+							if not (user_warehouse in d.t_warehouse or user_warehouse.replace("Normal","Breakage") in d.t_warehouse ):
+								frappe.throw(_("you cannot Receive stock from warehouse {0} .").format(d.t_warehouse))
+			else :
+				frappe.throw(_("your user has not allocated any warehouse ."))
 			
-				if ( strTransit in d.s_warehouse):
-						if not (user_warehouse in d.t_warehouse or user_warehouse.replace("Normal","Breakage") in d.t_warehouse ):
-							frappe.throw(_("you cannot Receive stock from warehouse {0} .").format(d.t_warehouse))
-		else :
-			frappe.throw(_("your user has not allocated any warehouse ."))
-		
 	
 @frappe.whitelist()
 def move_sample_to_retention_warehouse(company, items):
