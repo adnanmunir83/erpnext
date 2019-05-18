3e7280edab4deeef870836240231670bdaeb3078
Adnan <adnanmunir83@gmail.com>
2019-05-18 10:53:40 +0400

    Stock Entry Purpose fixed for otherthan material transfer

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -902,8 +902,9 @@ class StockEntry(StockController):
 					if ("Breakage" in d.t_warehouse and "Breakage" not in d.s_warehouse ):
 						frappe.throw(_("Normal Warehouse to Breakage Transfer is not possible for Item {0} .").format(d.item_code))
 
-				if ("Breakage" in d.s_warehouse and "Breakage" not in d.t_warehouse ):
-					frappe.throw(_("Breakage Warehouse to Normal Transfer is not possible for Item {0} .").format(d.item_code))
+				if d.s_warehouse and d.t_warehouse :
+					if ("Breakage" in d.s_warehouse and "Breakage" not in d.t_warehouse ):
+						frappe.throw(_("Breakage Warehouse to Normal Transfer is not possible for Item {0} .").format(d.item_code))
 			# Get User's Warehouse
 			
 			#check if user is owner of this warehouse
@@ -913,14 +914,16 @@ class StockEntry(StockController):
 			user_warehouse = frappe.db.get_value("User", {"name": frappe.session.user}, "user_warehouse")	
 			strTransit = "Transit"
 			if user_warehouse :	
-				for d in self.get("items"):				
-					if ( strTransit in d.t_warehouse ):
-						if not (user_warehouse in d.s_warehouse or user_warehouse.replace("Normal","Breakage") in d.s_warehouse or user_warehouse.replace("Depot","Breakage") in d.s_warehouse ):
-							frappe.throw(_("you are note allowed to tranfer stock from warehouse {0} .").format(d.s_warehouse))
+				for d in self.get("items"):	
+					if d.t_warehouse:
+						if ( strTransit in d.t_warehouse ):
+							if not (user_warehouse in d.s_warehouse or user_warehouse.replace("Normal","Breakage") in d.s_warehouse or user_warehouse.replace("Depot","Breakage") in d.s_warehouse ):
+								frappe.throw(_("you are note allowed to tranfer stock from warehouse {0} .").format(d.s_warehouse))
 				
-					if ( strTransit in d.s_warehouse):
-							if not (user_warehouse in d.t_warehouse or user_warehouse.replace("Normal","Breakage") in d.t_warehouse or user_warehouse.replace("Depot","Breakage") in d.t_warehouse ):
-								frappe.throw(_("you cannot Receive stock from warehouse {0} .").format(d.t_warehouse))
+					if d.s_warehouse:							
+						if ( strTransit in d.s_warehouse):
+								if not (user_warehouse in d.t_warehouse or user_warehouse.replace("Normal","Breakage") in d.t_warehouse or user_warehouse.replace("Depot","Breakage") in d.t_warehouse ):
+									frappe.throw(_("you cannot Receive stock from warehouse {0} .").format(d.t_warehouse))
 			else :
 				frappe.throw(_("your user has not allocated any warehouse ."))
 			
