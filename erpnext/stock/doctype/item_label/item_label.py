b21f7b85f3f11a517cf0f29c52a8cc13e99e631e
Adnan <adnanmunir83@gmail.com>
2019-12-07 14:44:11 +0400

    Label Issue fixed. JS code moved to PY

--- erpnext/stock/doctype/item_label/item_label.py
+++ erpnext/stock/doctype/item_label/item_label.py
@@ -10,8 +10,15 @@ class ItemLabel(Document):
 
 	def on_submit(self):
 		for d in self.get("items"):
-			if not frappe.db.get_value("Item Price", {"item_code": d.item_code,"price_list":d.price_list}):
+			# Check for Price list if it dosent exsist. create  a new one. # Get Item Price name			
+			item_price_name = frappe.db.get_value("Item Price", {"item_code": d.item_code,"price_list": d.price_list},"name")
+			if not item_price_name:
 				self.make_item_price(d.item_code,d.price_list,d.item_price)
+			else :				
+				old_item_price = frappe.db.get_value("Item Price", {"name": item_price_name},"price_list_rate")
+				# update Item Price if it's available and 			
+				if(old_item_price != d.item_price):
+					frappe.db.set_value("Item Price", item_price_name, "price_list_rate", d.item_price)
 
 	def make_item_price(self,item, price_list_name, item_price):
 		frappe.get_doc({
