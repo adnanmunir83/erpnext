07fd443919e6ff5cb3907eed8511f1fa71c35a72
Adnan <adnanmunir83@gmail.com>
2019-12-14 14:35:12 +0400

    Item Location Update doctype

---
+++ erpnext/stock/doctype/item_location/item_location.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class ItemLocation(Document):
+	def on_submit(self):
+		for d in self.get("items"):							
+			old_item_location = frappe.db.get_value("Item", {"name": d.item_code},"bin_location")
+			# update Item Price if it's available and 			
+			if(old_item_location != d.bin_location):
+				frappe.db.set_value("Item", d.item_code, "bin_location", d.bin_location)
+
+	
