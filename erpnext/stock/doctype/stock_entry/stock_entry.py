f9b2661faaed44cddb9488325274bd21e293eef2
Adnan <adnanmunir83@gmail.com>
2020-01-30 22:11:23 +0400

    Stock Entry Validation Removed for migration

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -920,10 +920,10 @@ class StockEntry(StockController):
 							if not (user_warehouse in d.s_warehouse or user_warehouse.replace("Normal","Breakage") in d.s_warehouse or user_warehouse.replace("Depot","Breakage") in d.s_warehouse ):
 								frappe.throw(_("you are note allowed to tranfer stock from warehouse {0} .").format(d.s_warehouse))
 				
-					if d.s_warehouse:							
-						if ( strTransit in d.s_warehouse):
-								if not (user_warehouse in d.t_warehouse or user_warehouse.replace("Normal","Breakage") in d.t_warehouse or user_warehouse.replace("Depot","Breakage") in d.t_warehouse ):
-									frappe.throw(_("you cannot Receive stock from warehouse {0} .").format(d.t_warehouse))
+					# if d.s_warehouse:							
+					# 	if ( strTransit in d.s_warehouse):
+					# 			if not (user_warehouse in d.t_warehouse or user_warehouse.replace("Normal","Breakage") in d.t_warehouse or user_warehouse.replace("Depot","Breakage") in d.t_warehouse ):
+					# 				frappe.throw(_("you cannot Receive stock from warehouse {0} .").format(d.t_warehouse))
 			else :
 				frappe.throw(_("your user has not allocated any warehouse ."))
 			
